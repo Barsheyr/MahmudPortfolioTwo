4189146c2a3e13063c6e2408d7dbce0b4613401f
Bashir Amokomowo <barsheyr@gmail.com>
2024-07-09 12:22:22 +0100

    push

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const ContactPage = () => {
-  return <div>ContactPage</div>;
+  return (
+    <section className="py-32">
+      <div>Contact Page loading...........</div>
+    </section>
+  );
 };
 
 export default ContactPage;
